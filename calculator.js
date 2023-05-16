b69ce108f0ab0d6cdb75fbfc846ccb3811ef665c
Javaeria <javerianisar.1993@gmail.com>
2023-05-16 17:15:22 +1000

    Create js script file with initial values

---
+++ calculator.js
@@ -0,0 +1,9 @@
+const clearButton = document.querySelector(".clear");
+const deleteButton = document.querySelector(".delete");
+const calcButtonsList = document.querySelectorAll(".calc-buttons");
+const enteredValue = document.querySelector("#entered-value");
+const resultValue = document.querySelector("#result-value");
+let aValue = "";
+let bValue = "";
+let result;
+let currentOperand = "";
