7b7b673c42d945866aaa9d02ba1e969748647639
Javaeria <javerianisar.1993@gmail.com>
2023-05-16 17:22:09 +1000

    Add interpret function with buttons listeners using this function as a callback

--- calculator.js
+++ calculator.js
@@ -33,10 +33,69 @@ deleteButton.addEventListener("click", () => {
   }
 });
 
+calcButtonsList.forEach((button) => {
+  button.addEventListener("click", () => interpretButtons(button.textContent));
+});
+
+function interpretButtons(buttonText) {
+  switch (true) {
+    case (buttonText >= 0 && buttonText <= 9) || buttonText === ".":
+      if (currentOperand === "") {
+        aValue += buttonText;
+        enteredValue.textContent += buttonText;
+      } else {
+        bValue += buttonText;
+        enteredValue.textContent = bValue;
+      }
+      break;
+    case buttonText in methods:
+      if (aValue !== "" && bValue !== "") evaluateResult(buttonText, true);
+      else {
+        resultValue.textContent = `${aValue} ${buttonText}`;
+        currentOperand = buttonText;
+      }
+      break;
+    case buttonText === "x":
+      if (aValue !== "" && bValue !== "") evaluateResult("x", true);
+      else {
+        resultValue.textContent = `${aValue} ${buttonText}`;
+      }
+      currentOperand = "*";
+      break;
+    case buttonText === "=":
+      if (bValue) {
+        evaluateResult("=", false);
+      }
+      break;
+  }
+}
+
 roundToOneDecimal = (number) => Math.round(number * 10) / 10;
 
 removeDigit = (number) => {
   number = number.slice(0, -1);
   if (number[number.length - 1] === ".") number = number.slice(0, -1);
   return number;
+};
+
+evaluateResult = (buttonValue, replaceScreenValue) => {
+  if (currentOperand === "÷" && bValue === "0") {
+    alert("You can't divide by 0!");
+    return;
+  }
+  result = roundToOneDecimal(methods[currentOperand](aValue, bValue));
+  if (result === Infinity || Number.isNaN(result)) {
+    alert("Invalid Input !");
+    return;
+  }
+  enteredValue.textContent = result;
+  if (replaceScreenValue) {
+    resultValue.textContent = ` ${result} ${buttonValue}`;
+    currentOperand = buttonValue;
+  } else {
+    resultValue.textContent += ` ${bValue} ${buttonValue}`;
+    currentOperand = "";
+  }
+  aValue = result;
+  bValue = "";
 };
